387f0d89fadd11ea302bda5ebbf2c2296cc6cd5f
mikka <ezsolt@gmail.com>
2020-10-24 19:23:20 +0200

    fake database for testing domain payloads

---
+++ app/BlackHole/FakeDB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Fake database for testing
+ */
+
+return [
+    'page' => [
+        'title' => 'Stardust Article Title',
+        'metaTitle' => 'Stardust Article',
+        'description' => 'Sample Stardust Article Text',
+        'date' => '2020-20',
+        'slug' => 'stardust-article-title',
+        'author' => 'ubik',
+        'categories' => 'Mikka,Makka,Zorro'
+    ]
+];
