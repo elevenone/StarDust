41aaa0490effb4ab7514d5abe2aabf1a8406fafd
mikka <ezsolt@gmail.com>
2020-10-25 18:01:56 +0100

    new payload responder

--- app/Responder/PayloadResponder.php
+++ app/Responder/PayloadResponder.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace App\Responder;
 
 use DarkMatter\Http\Response;
-use DarkMatter\Responder\JsonResponder;
-use DarkMatter\Responder\HtmlResponder;
 use DarkMatter\Payload\Payload;
 use DarkMatter\Payload\Status;
+
+use DarkMatter\Responder\HtmlResponder;
+
+
+/**
+ * @property string $view
+ */
 
 class PayloadResponder extends HtmlResponder
 {
@@ -21,10 +26,19 @@ class PayloadResponder extends HtmlResponder
     public function __invoke(Payload $payload): Response
     {
         $payloadResult = $payload->getResult();
+
         if ($payload->getStatus() === Status::FOUND) {
             return $this->found($payloadResult);
         }
 
         return $this->error([$payloadResult['error'] ?? 'Unknown Error']);
     }
+
+
+
+
+
+
+
+
 }
